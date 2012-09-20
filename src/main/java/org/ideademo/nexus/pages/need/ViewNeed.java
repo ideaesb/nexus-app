2f6ba92eef27e48da93e60cf88810d7eacc7e90e
IDEA ESB <ideaesb@gmail.com>
2012-09-19 17:37:36 -1000

    update to Example search, layout css

--- src/main/java/org/ideademo/nexus/pages/need/ViewNeed.java
+++ src/main/java/org/ideademo/nexus/pages/need/ViewNeed.java
@@ -1,5 +1,18 @@
 package org.ideademo.nexus.pages.need;
 
-public class ViewNeed {
+import org.apache.tapestry5.annotations.PageActivationContext;
+import org.apache.tapestry5.annotations.Property;
 
+import org.ideademo.nexus.entities.Need;
+
+public class ViewNeed 
+{
+
+  @PageActivationContext 
+  @Property
+  private Need entity;
+	  
+	  
+  void onPrepareForRender()  {if(this.entity == null){this.entity = new Need();}}
+  void onPrepareForSubmit()  {if(this.entity == null){this.entity = new Need();}}
 }
